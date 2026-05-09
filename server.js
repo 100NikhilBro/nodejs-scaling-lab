7dad7c8c7b6aeb931d3f2eddbd17b73905ec27c9
Nikhil Gupta <100pranjalgupta@gmail.com>
2026-05-09 22:58:35 +0530

    initial setup

---
+++ server.js
@@ -0,0 +1,37 @@
+const { config } = require('dotenv');
+config();
+
+const express = require('express');
+const app = express();
+
+const morgan = require('morgan');
+app.use(morgan('dev'));
+
+const pino = require('pino');
+const logger = pino();
+
+const pinoHttp = require('pino-http');
+app.use(pinoHttp());
+
+const PORT = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+
+    logger.info('Home Route hit');
+
+    let s = 0;
+
+    for(let i = 0; i < 5e7; i++){
+        s = s + i;
+    }
+
+    return res.json({
+        Server: "Nginx-Server",
+        Health: "Good",
+        sum: s
+    });
+});
+
+app.listen(PORT, () => {
+    logger.info(`Server started on ${PORT}`);
+});
